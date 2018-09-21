051375fb28475ea79b21327154e304d2a6f9fde6
plaxi <34620998+plaxi@users.noreply.github.com>
2018-09-21 03:49:12 +0200

    Add files via upload

---
+++ programs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo pacman -S gvfs archey3 vlc libreoffice-fresh-sv grsync ntfs-3g leafpad virtualbox chromium qalculate --noconfirm
+sudo pacman -S xorg-server xorg-xinit xorg-xclock xterm
+sudo pacman -S plasma kde-applications
